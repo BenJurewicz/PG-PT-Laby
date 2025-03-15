38239f8891024d59a2b7101cb278a0cf512f9d6c
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-03-15 21:19:32 +0100

    Change project name to the something that makes more sense and update gitignore

--- src/main/java/jkz/App.java
+++ src/main/java/jkz/App.java
@@ -1,10 +1,6 @@
-package ToolsQA;
+package jkz;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
--- src/test/java/jkz/AppTest.java
+++ src/test/java/jkz/AppTest.java
@@ -1,4 +1,4 @@
-package ToolsQA;
+package jkz;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
