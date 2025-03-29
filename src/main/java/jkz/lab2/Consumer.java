cf6c62a55fa93ce3708abb6b2ea5789207503934
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-03-30 00:19:18 +0100

    Set the interval for NumberGenerator, compact Answer.toString, add answer id when printing in Consumer

--- src/main/java/jkz/lab2/Consumer.java
+++ src/main/java/jkz/lab2/Consumer.java
@@ -3,35 +3,38 @@ package jkz.lab2;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Consumer implements Runnable {
-    private final BlockingQueue<Answer> answers;
-    private final AtomicBoolean stopSignal;
+	static AtomicLong COUNT = new AtomicLong(0);
+	private final BlockingQueue<Answer> answers;
+	private final AtomicBoolean stopSignal;
 
-    public Consumer(BlockingQueue<Answer> answers, AtomicBoolean stopSignal) {
-        this.answers = answers;
-        this.stopSignal = stopSignal;
-    }
+	public Consumer(BlockingQueue<Answer> answers, AtomicBoolean stopSignal) {
+		this.answers = answers;
+		this.stopSignal = stopSignal;
+	}
 
-    public Answer tryTake() {
-        try {
-            return answers.poll(500, TimeUnit.MILLISECONDS);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public Answer tryTake() {
+		try {
+			return answers.poll(500, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public void handleAnswer(Answer answer) {
-        System.out.println(answer);
-    }
+	public void handleAnswer(Answer answer) {
+		System.out.print(COUNT.incrementAndGet() + ":  ");
+		System.out.println(answer);
+	}
 
-    @Override
-    public void run() {
-        while (!stopSignal.get() || !answers.isEmpty()) {
-            Answer answer = tryTake();
-            if (answer != null) {
-                handleAnswer(answer);
-            }
-        }
-    }
+	@Override
+	public void run() {
+		while (!stopSignal.get() || !answers.isEmpty()) {
+			Answer answer = tryTake();
+			if (answer != null) {
+				handleAnswer(answer);
+			}
+		}
+	}
 }
