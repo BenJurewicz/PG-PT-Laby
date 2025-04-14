ef018eb03dfa6633c2d4223325eb2ccce59aa87a
Marta Kociszewska <martakociszewska04@gmail.com>
2025-04-14 12:11:50 +0200

    formatted output

--- Lab5/src/main/java/jkz/Requests.java
+++ Lab5/src/main/java/jkz/Requests.java
@@ -12,7 +12,7 @@ public class Requests {
         Manager<Employee> employeeManager = new Manager<>(Employee.class);
         String sqlQuery = "SELECT e.name AS EmployeeName, d.name AS DepartmentName, e.employedFrom FROM Employee e JOIN Department d ON e.department_id = d.id WHERE e.employedFrom = (SELECT MIN(employedFrom) FROM Employee WHERE department_id = e.department_id);";
         List<?> result = employeeManager.sql(sqlQuery);
-        Log.debug("Candidates for raise: ", result.toString());
+        Log.debug("Candidates for raise: ");
 
 //        prettyPrintQueryResult(result, "Names");
         prettyPrintQueryResult(result);
