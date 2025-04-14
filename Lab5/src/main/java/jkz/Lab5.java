60e50cdb9978be57670a7af69d680cdcae88d2c2
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-04-14 03:42:13 +0200

    Add example usage and silence logging from the database

--- Lab5/src/main/java/jkz/Lab5.java
+++ Lab5/src/main/java/jkz/Lab5.java
@@ -1,7 +1,45 @@
 package jkz;
 
+import jkz.Database.Entities.Company;
+import jkz.Database.Entities.Department;
+import jkz.Database.Entities.Employee;
+import jkz.Database.Managers.Manager;
+
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class Lab5 {
+
 	public static void main(String[] args) {
-		System.out.println("Lab 5");
+		silenceLogging();
+
+		Manager<Company> companyManager = new Manager<>(Company.class);
+		Manager<Department> departmentManager = new Manager<>(Department.class);
+		Manager<Employee> employeeManager = new Manager<>(Employee.class);
+
+		Company company = new Company("Google", "Technology");
+		companyManager.add(company);
+
+		Department department = new Department("Engineering", "London", company);
+		departmentManager.add(department)
+		;
+		Employee employee = new Employee("John Doe", "123456789", 1000, new Date(), department);
+		employeeManager.add(employee);
+		department.setManager(employee);
+		departmentManager.update(department);
+
+//		employeeManager.remove(employee.getId());
+//		departmentManager.remove(department.getId());
+//		companyManager.remove(company.getId());
+	}
+
+	public static void silenceLogging() {
+		Logger rootLogger = LogManager.getLogManager().getLogger("");
+		rootLogger.setLevel(Level.SEVERE);
+
+		Logger hibernateLogger = Logger.getLogger("org.hibernate");
+		hibernateLogger.setLevel(Level.SEVERE);
 	}
 }
