1928eaf2e69069c401b0ee5004f2c581d1eb5dbb
Marta Kociszewska <martakociszewska04@gmail.com>
2025-04-14 10:25:36 +0200

    changed query to correct names

--- Lab5/src/main/java/jkz/Requests.java
+++ Lab5/src/main/java/jkz/Requests.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Requests {
     public static List<?>  getCandidatesForRaise(){
         Manager<Employee> employeeManager = new Manager<>(Employee.class);
-        String sqlQuery = "SELECT e.name AS EmployeeName, d.name AS DepartmentName, e.employedFrom FROM Employee e JOIN Department d ON e.department_id = d.id WHERE e.employment_date = (SELECT MIN(employment_date) FROM Employee WHERE department_id = e.department_id);";
+        String sqlQuery = "SELECT e.name AS EmployeeName, d.name AS DepartmentName, e.employedFrom FROM Employee e JOIN Department d ON e.department_id = d.id WHERE e.employedFrom = (SELECT MIN(employedFrom) FROM Employee WHERE department_id = e.department_id);";
         List<?> result = employeeManager.sql(sqlQuery);
         Log.debug("Candidates for raise: ", result.toString());
         return result;
