79e9595b88f4480dabdf6224d7673494e9e24fd3
Marta Kociszewska <martakociszewska04@gmail.com>
2025-04-14 11:41:38 +0200

    updated one query

--- Lab5/src/main/java/jkz/Requests.java
+++ Lab5/src/main/java/jkz/Requests.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Requests {
     public static List<?>  getCandidatesForRaise(){
         Manager<Employee> employeeManager = new Manager<>(Employee.class);
-        String sqlQuery = "SELECT name as Names FROM Employee";
+        String sqlQuery = "SELECT e.name AS EmployeeName, d.name AS DepartmentName, e.employedFrom FROM Employee e JOIN Department d ON e.department_id = d.id WHERE e.employedFrom = (SELECT MIN(employedFrom) FROM Employee WHERE department_id = e.department_id);";
         List<?> result = employeeManager.sql(sqlQuery);
         Log.debug("Candidates for raise: ", result.toString());
 
