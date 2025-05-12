0994b6f0c292bcf9e2c7ebd4125b70526a036047
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-05-12 09:53:25 +0200

    Initial commit of Lab7

---
+++ Lab7/Human.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+	public enum Gender {
+		Male,
+		Female,
+		Other
+	}
+	public class Human {
+		public string Name { get; set; }
+		public Gender Gender { get; set; }
+		public int Age { get; set; }
+		
+		public HashSet<Human> humans { get; set; }
+
+		public Human() { 
+			Name = String.Empty;
+			humans = new HashSet<Human>(); 
+		}
+
+		public Human(string name, Gender gender, int age)
+			: this()
+		{
+			Name = name;
+			Gender = gender;
+			Age = age;
+		}
+
+		public bool AddChild(Human child)
+		{
+			return humans.Add(child);
+		}
+
+		public override string ToString() {
+			return $"Human: {Name}, {Gender}, {Age}";
+		}
+	}
+
+}
---
+++ Lab7/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public ObservableCollection<Human> humans { get; set; } = new ObservableCollection<Human>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            this.GenerateDataMenuItem_Click(this, null);
+        }
+
+        private void GenerateDataMenuItem_Click(object sender, RoutedEventArgs? e)
+        {
+            humans.Clear();
+            HashSet<Human> humanSet = HumanGenerator.Generate(6);
+            foreach (var human in humanSet)
+            {
+                humans.Add(human);
+            }
+            //MessageBox.Show($"Generated {humans.Count} humans.");
+        }
+
+        private void VersionMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Version appVersion = Assembly.GetExecutingAssembly().GetName().Version!;
+            MessageBox.Show($"Version menu item clicked\n The version is: {appVersion}");
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
